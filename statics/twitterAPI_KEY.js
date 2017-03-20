011777e38833a8c7839bf3e80911a22be616beaa
Chris Wang <chris_wang@icloud.com>
2017-03-20 01:34:32 +0000

    removed redundant code

--- statics/twitterAPI_KEY.js
+++ statics/twitterAPI_KEY.js
@@ -1,13 +1,8 @@
 var Twitter = require('twit');
 
-var TWITTER_CONSUMER_KEY = 'YOUR KEY HERE';
-var TWITTER_CONSUMER_SECRET = 'YOUR KEY HERE';
-var TWITTER_ACCESS_TOKEN_KEY = 'YOUR KEY HERE';
-var TWITTER_ACCESS_TOKEN_SECRET = 'YOUR KEY HERE';
-
 module.exports = new Twitter({
-    consumer_key: TWITTER_CONSUMER_KEY,
-    consumer_secret: TWITTER_CONSUMER_SECRET,
-    access_token: TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: 'YOUR KEY HERE',
+    consumer_secret: 'YOUR KEY HERE',
+    access_token: 'YOUR KEY HERE',
+    access_token_secret: 'YOUR KEY HERE'
 });
