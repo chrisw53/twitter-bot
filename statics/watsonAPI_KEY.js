1e7925c8c6550a47e26100afaf9be0f2e77852b4
Chris Wang <chris_wang@icloud.com>
2017-03-21 16:55:55 +0000

    fixed typo in watson API Key

--- statics/watsonAPI_KEY.js
+++ statics/watsonAPI_KEY.js
@@ -3,6 +3,6 @@ var Watson = require('watson-developer-cloud/conversation/v1');
 module.exports = new Watson({
     username: process.env.WATSON_USERNAME,
     password: process.env.WATSON_PASSWORD,
-    path: { workspace_id: process.env.WATSON_WORKPLACE_ID },
+    path: { workspace_id: process.env.WATSON_WORKSPACE_ID },
     version_date: Watson.VERSION_DATE_2017_02_03
 });
