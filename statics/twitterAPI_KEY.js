774f90e4f2f6c45d8e2e96d88cec1bb1a673a1f5
Chris Wang <chris_wang@icloud.com>
2017-03-21 00:30:23 +0000

    changes to API_KEY files

--- statics/twitterAPI_KEY.js
+++
@@ -1,8 +0,0 @@
-var Twitter = require('twit');
-
-module.exports = new Twitter({
-    consumer_key: 'YOUR KEY HERE',
-    consumer_secret: 'YOUR KEY HERE',
-    access_token: 'YOUR KEY HERE',
-    access_token_secret: 'YOUR KEY HERE'
-});
--- statics/watsonAPI_KEY.js
+++
@@ -1,8 +0,0 @@
-var Watson = require('watson-developer-cloud/conversation/v1');
-
-module.exports = new Watson({
-    username: 'YOUR KEY HERE',
-    password: 'YOUR KEY HERE',
-    path: { workspace_id: 'YOUR KEY HERE' },
-    version_date: Watson.VERSION_DATE_2017_02_03
-});
